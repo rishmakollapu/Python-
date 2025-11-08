90cdb0ae107b6fb390f2f1c7751870de242c79ab
rishmakollapu <rishmakollapu@gmail.com>
2025-11-08 08:26:54 +0530

    Week_4

---
+++ ex_4.1(a).py
@@ -0,0 +1,7 @@
+import pandas as pd
+# Creating a Pandas Series
+data = [10, 20, 30, 40, 50]
+series = pd.Series(data)
+print("Pandas Series:")
+print(series)
+
---
+++ ex_4.1(b).py
@@ -0,0 +1,4 @@
+# Convert Series to Python list
+list_data = series.tolist()
+print("Series as Python List:", list_data)
+print("Type of list_data:", type(list_data))
